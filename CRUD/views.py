c397218865129a3250997a926f6c5cd6bfdc6f81
muzahid62 <muzahidul15-2559@diu.edu.bd>
2022-10-27 21:59:11 +0600

    feat: Entry Forms.
    create forms.py file and Forms are created based on the model.Show forms 
    in album_form and musician_form page. And both forms are submit

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from CRUD.models import Musician, Album
+from CRUD import forms
+
 # Create your views here.
 def index(request):
-    diction = {'title':'Home Page'}
+    musician_list = Musician.objects.order_by("first_name")
+    diction = {'title':'Home Page', 'musician_list':musician_list}
     return render(request,'CRUD/index.html',context=diction)
 
 def album_list(request):
@@ -10,9 +14,26 @@ def album_list(request):
     return render(request,'CRUD/album_list.html',context=diction)
 
 def musician_form(request):
-    diction = {'title':'Add New Musician'}
+    form = forms.MusicianForm()
+
+    if request.method == 'POST':
+        form = forms.MusicianForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    diction = {'title':'Add New Musician', 'musician_form':form}
     return render(request,'CRUD/musician_form.html',context=diction)
 
 def album_form(request):
-    diction = {'title':'Add New Album'}
+    form = forms.AlbumForm()
+
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+    diction = {'title':'Add New Album', 'album_form':form}
     return render(request,'CRUD/album_form.html',context=diction)
