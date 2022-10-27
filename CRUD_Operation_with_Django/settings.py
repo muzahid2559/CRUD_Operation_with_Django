740b22542587d1dd2373b40a5c491d3d74d973a5
muzahid62 <muzahidul15-2559@diu.edu.bd>
2022-10-27 14:19:51 +0600

    feat: connecting mysql data.

--- CRUD_Operation_with_Django/settings.py
+++ CRUD_Operation_with_Django/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'CRUD_Operation_with_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
