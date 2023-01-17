2c9adead15ecd04662c3d3c118a5dc94596fd6b3
muzahid626 <muzahidul15-2559@diu.edu.bd>
2023-01-17 15:42:35 +0600

    admin-site name edit

--- CRUD/admin.py
+++ CRUD/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from CRUD.models import Musician, Album
+
+from CRUD.models import Album, Musician
+
 # Register your models here.
 admin.site.register(Musician)
 admin.site.register(Album)
+
+
+
+admin.site.site_header = "CRUD Admin"
+admin.site.site_title = "CRUD Admin Portal"
+admin.site.index_title = "Welcome to CRUD Admin Portal"
